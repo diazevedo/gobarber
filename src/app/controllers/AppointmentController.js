4e6137dd276a87c5a8e445d6907442252a66ea61
Diego Azevedo <dapptn@hotmail.com>
2019-08-26 11:04:29 +1000

    Registering appointment in database.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,14 +1,35 @@
+import * as Yup from 'yup';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
 class AppointmentController {
   async store(req, res) {
-    const userAppointments = await Appointment.findAll({
-      where: {
-        user_id: req.userId,
-      },
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
     });
-    res.json(userAppointments);
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Error' });
+
+    const { provider_id, date } = req.body;
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'The user sent is not a provider.' });
+    }
+
+    const userAppointments = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+    return res.json(userAppointments);
   }
 }
 
