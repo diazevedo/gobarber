577c5f71ddd20d911238d84f8186cc15aaed5efc
Diego Azevedo <dapptn@hotmail.com>
2019-08-27 09:15:45 +1000

    List of provider appointments of the current day

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import checkTokenMD from './app/middlewares/jwt';
 import ProviderController from './app/controllers/ProviderController';
 import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleProvider from './app/controllers/ScheduleProvider';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,5 +30,6 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments/', AppointmentController.index);
+routes.get('/schedule/provider', ScheduleProvider.index);
 
 export default routes;
