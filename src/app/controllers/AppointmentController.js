b753f4bd687b4f9d254ffbffd75950b51bd84533
Diego Azevedo <dapptn@hotmail.com>
2019-08-26 11:18:44 +1000

    Data validations added

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -23,6 +24,21 @@ class AppointmentController {
         .status(401)
         .json({ error: 'The user sent is not a provider.' });
     }
+    /**
+     * iso transforms the date to a JS object date
+     * start Hour removes any minute or seconds.
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date()))
+      return res.status(401).json({ error: 'Past dates are not permitted.' });
+
+    const checkAvailitiby = await Appointment.findOne({
+      where: { provider_id, date: hourStart, cancelled_at: null },
+    });
+
+    if (checkAvailitiby)
+      return res.status(401).json({ error: 'Date is not unavailable.' });
 
     const userAppointments = await Appointment.create({
       user_id: req.userId,
