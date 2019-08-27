972c2b838cfd9a051e583c9151b46eb00def2c7a
Diego Azevedo <dapptn@hotmail.com>
2019-08-27 15:17:25 +1000

    Queue to process sending email service

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,8 +6,9 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../../schemas/Notification';
+import CancellationMail from '../jobs/CancellationMail';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -142,20 +143,8 @@ class AppointmentController {
     }
 
     appointment.cancelled_at = new Date();
+    Queue.add(CancellationMail.key, { appointment });
     await appointment.save();
-
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "dd 'de' MMMM', às 'H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
 
     return res.json(appointment);
   }
