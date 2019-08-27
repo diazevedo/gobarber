f18e35eaa4e86daa47f5eb903b34d5dddc75585a
Diego Azevedo <dapptn@hotmail.com>
2019-08-27 10:08:44 +1000

    Mongo added, created and saving notification on it

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -75,6 +77,17 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    const { name } = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "dd 'de' MMMM', às 'H:mm'h'", {
+      locale: pt,
+    });
+
+    /** Notify provider */
+    await Notification.create({
+      content: `Novo agendamento de ${name} para o dia ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(userAppointment);
