a48d76bcff336f58f07fb99d684c3cbdb9126cd7
Diego Azevedo <dapptn@hotmail.com>
2020-08-05 17:13:01 +1000

    teste with only origin

--- src/app.js
+++ src/app.js
@@ -22,25 +22,13 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
 
-    const whitelist = [
-      'https://digobarber.netlify.app',
-      'https://www.gobarber.dideveloper.com/',
-      'https://gobarber.dideveloper.com/',
-    ];
-
-    const corsOptions = {
-      origin(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      optionsSuccessStatus: 200,
-    };
-
     if (process.env.NODE_ENV === 'production') {
-      this.server.use(cors(corsOptions));
+      this.server.use(
+        cors({
+          origin: 'https://www.gobarber.dideveloper.com',
+          optionsSuccessStatus: 200,
+        })
+      );
     } else {
       this.server.use(cors());
     }
