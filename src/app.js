4ba4c34ebf274a4ba188bd152fb5851870f0ddad
Diego Azevedo <dapptn@hotmail.com>
2020-08-05 17:04:04 +1000

    origin added to cors

--- src/app.js
+++ src/app.js
@@ -22,13 +22,25 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
 
+    const whitelist = [
+      'https://digobarber.netlify.app',
+      'https://www.gobarber.dideveloper.com/',
+      'https://gobarber.dideveloper.com/',
+    ];
+
+    const corsOptions = {
+      origin(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      optionsSuccessStatus: 200,
+    };
+
     if (process.env.NODE_ENV === 'production') {
-      this.server.use(
-        cors({
-          origin: 'https://digobarber.netlify.app',
-          optionsSuccessStatus: 200,
-        })
-      );
+      this.server.use(cors(corsOptions));
     } else {
       this.server.use(cors());
     }
